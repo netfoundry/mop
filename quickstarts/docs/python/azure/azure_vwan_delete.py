9f6f6f60ef9b0b920e11b7ed4c2cb101386cd79b
dariuszSki <dariusz.sliwinski@netfoundry.io>
2019-12-18 13:49:20 -0500

    seperated vpn gateway creation from avw site creation

--- quickstarts/docs/python/azure/azure_vwan_delete.py
+++ quickstarts/docs/python/azure/azure_vwan_delete.py
@@ -15,20 +15,6 @@ credentials = ServicePrincipalCredentials(
 # Connect to Azure APIs and get session details
 network_client = NetworkManagementClient(credentials, os.environ.get('ARM_SUBSCRIPTION_ID'))
 
-# Delete Server VM
-
-# Delete VPNG
-async_vpng_deletion = network_client.vpn_gateways.delete(
-    os.environ.get('GROUP_NAME'),
-    os.environ.get('VPNG_NAME'),
-    custom_headers=None,
-    raw=False,
-    polling=True
-)
-async_vpng_deletion.wait()
-print(async_vpng_deletion.result())
-print('VPNG Deleted')
-
 # Delete VHUB
 async_vhub_deletion = network_client.virtual_hubs.delete(
     os.environ.get('GROUP_NAME'),

57c50da4145a86d53c6cb3ba361e8a32be02044d
dariuszSki <dariusz.sliwinski@netfoundry.io>
2019-12-19 19:08:24 -0500

    crated top level script

---
+++ quickstarts/docs/python/nfn_create.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import os
+import nf_token as nft
+import nf_network as nfn
+nft.clear_logs()
+token=nft.get_token(os.environ.get('ENVIRONMENT'))
+nfn_url=nfn.create_network(os.environ.get('ENVIRONMENT'), os.environ.get('NFN_NAME'), token)
+print(nfn_url)

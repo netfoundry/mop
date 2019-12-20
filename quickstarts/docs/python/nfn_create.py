a4d71eea54a38473ff4982055668c9ac98abf31c
dariuszSki <dariusz.sliwinski@netfoundry.io>
2019-12-19 19:09:29 -0500

    syntax

--- quickstarts/docs/python/nfn_create.py
+++ quickstarts/docs/python/nfn_create.py
@@ -3,7 +3,7 @@
 import os
 import nf_token as nft
 import nf_network as nfn
-nft.clear_logs()
+nft.clear_log()
 token=nft.get_token(os.environ.get('ENVIRONMENT'))
 nfn_url=nfn.create_network(os.environ.get('ENVIRONMENT'), os.environ.get('NFN_NAME'), token)
 print(nfn_url)

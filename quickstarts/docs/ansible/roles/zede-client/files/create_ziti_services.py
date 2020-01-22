2a87170cf3f6c9093a0ab7cfe42897ef7b17bb6d
dariuszSki <dariusz.sliwinski@netfoundry.io>
2020-01-21 22:04:36 -0500

    updated python script to configure services

--- quickstarts/docs/ansible/roles/zede-client/files/create_ziti_services.py
+++ quickstarts/docs/ansible/roles/zede-client/files/create_ziti_services.py
@@ -67,13 +67,18 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true',
                         help='enable debug log in log file output')
-    parser.add_argument('-v', '--version', action='version', version=__version__)
-    parser.add_argument('-u', '--username', default='admin', help='controller username, default is admin')
-    parser.add_argument('-p', '--password', required='yes', help='controller password')
-    parser.add_argument('-cip', '--controller-ip', dest='controllerIp', required='yes', help='controller ip')
+    parser.add_argument('-v', '--version', action='version',
+                        version=__version__)
+    parser.add_argument('-u', '--username', default='admin',
+                        help='controller username, default is admin')
+    parser.add_argument('-p', '--password', required='yes',
+                        help='controller password')
+    parser.add_argument('-cip', '--controller_ip',
+                        required='yes', help='controller ip')
     parser.add_argument('--cluster_name', help='ingress cluster name')
     parser.add_argument('--gateway_name', help='edge gateway name')
     parser.add_argument('--identity_name', help='identity name')
+    parser.add_argument('--service_dns', help='service ip or dns')
     parser.add_argument('-ca', '--ca-chain', dest='ca_chain',
                         help='ca chain for the controller')
     # get arguments passed to this module
@@ -85,8 +90,8 @@ def main():
         log_level = logging.INFO
     # configure logging
     try:
-        logging.basicConfig(filename=os.path.expanduser("~/update_indentity.log"),
-                            format='%(asctime)s-identity-%(levelname)s,%(message)s',
+        logging.basicConfig(filename=os.path.expanduser("logoutput.txt"),
+                            format='%(asctime)s-ziti-%(levelname)s,%(message)s',
                             datefmt='%Y-%m-%d-%H:%M:%S',
                             level=log_level)
     except Exception as excpt:
@@ -96,8 +101,9 @@ def main():
     public_key = args.ca_chain
     # login to ontroller to gain a session token
     try:
-        url = 'https://%s:1280/authenticate?method=password' % args.controllerIp
-        payload = "{\n  \"username\": \"%s\",\n  \"password\": \"%s\"\n}" % (args.username, args.password)
+        url = 'https://%s:1280/authenticate?method=password' % args.controller_ip
+        payload = "{\n  \"username\": \"%s\",\n  \"password\": \"%s\"\n}" \
+                    % (args.username, args.password)
         content_type = {"content-type": "application/json"}
         data = post_request(url, content_type, payload, public_key)
         session_token = data[0]['token']
@@ -107,43 +113,55 @@ def main():
         logging.debug(traceback.format_exc())
     # find ingress cluster
     try:
-        url = 'https://%s:1280/clusters' % args.controllerIp
-        content_type = {"content-type": "application/json", "zt-session": session_token}
+        url = 'https://%s:1280/clusters' % args.controller_ip
+        content_type = {"content-type": "application/json", \
+                        "zt-session": session_token}
         data = get_request(url, content_type, public_key)
-        clusterId = data[0]['id']
+        for cluster in data[0]:
+            if cluster['name'] == args.cluster_name:
+                clusterId = cluster['id']
         logging.info(data[1])
     except Exception as excpt:
         logging.error(str(excpt))
         logging.debug(traceback.format_exc())
     # find ingress cluster
     try:
-        url = 'https://%s:1280/gateways' % args.controllerIp
-        content_type = {"content-type": "application/json", "zt-session": session_token}
+        url = 'https://%s:1280/gateways' % args.controller_ip
+        content_type = {"content-type": "application/json", \
+                        "zt-session": session_token}
         data = get_request(url, content_type, public_key)
-        gatewayId = data[0]['id']
+        for gateway in data[0]:
+            if gateway['name'] == args.gateway_name:
+                gatewayId = gateway['id']
         logging.info(data[1])
     except Exception as excpt:
         logging.error(str(excpt))
         logging.debug(traceback.format_exc())
     # find an identity to be assigned to appWan
     try:
-        url = 'https://%s:1280/identities' % args.controllerIp
-        content_type = {"content-type": "application/json", "zt-session": session_token}
+        url = 'https://%s:1280/identities' % args.controller_ip
+        content_type = {"content-type": "application/json", \
+                        "zt-session": session_token}
         data = get_request(url, content_type, public_key)
-        identityId = data[0]['id']
+        for identity in data[0]:
+            if identity['name'] == args.identity_name:
+                identityId = identity['id']
         logging.info(data[1])
     except Exception as excpt:
         logging.error(str(excpt))
         logging.debug(traceback.format_exc())
-    # create a service to be assigned to appWan
     try:
-        url = 'https://%s:1280/services' % args.controllerIp
+        url = 'https://%s:1280/services' % args.controller_ip
         payload = "{\"name\":\"iperf3\",\"protocols\": [\"tls\"], \
                     \"dns\":{\"hostname\":\"%s\",\"port\": 5201}, \
                     \"egressRouter\":\"%s\", \
                     \"endpointAddress\":\"tcp://%s:5201\", \
-                    \"clusters\": [\"%s\"]}" % (args.serverIp, gatewayId, args.serverIp, clusterId)
-        content_type = {"content-type": "application/json", "zt-session": session_token}
+                    \"clusters\": [\"%s\"]}" % (args.service_dns,
+                                                gatewayId,
+                                                args.service_dns,
+                                                clusterId)
+        content_type = {"content-type": "application/json", \
+                        "zt-session": session_token}
         data = post_request(url, content_type, payload, public_key)
         serviceId = data[0]['id']
         logging.info(data[1])
@@ -152,10 +170,11 @@ def main():
         logging.debug(traceback.format_exc())
     # create AppWan
     try:
-        url = 'https://%s:1280/app-wans' % (args.controllerIp)
+        url = 'https://%s:1280/app-wans' % (args.controller_ip)
         payload = "{\"name\":\"appwan01\",\"identities\":[\"%s\"], \
-                    \"services\":[\%s\"]}" % (identityId, serviceId)
-        content_type = {"content-type": "application/json", "zt-session": session_token}
+                    \"services\":[\"%s\"]}" % (identityId, serviceId)
+        content_type = {"content-type": "application/json", \
+                        "zt-session": session_token}
         data = post_request(url, content_type, payload, public_key)
         apWanId = data[0]['id']
         logging.info(data[1])

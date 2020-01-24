a580faf03c215b6a3603d61e1d14d9dd600c00f4
dariuszSki <dariusz.sliwinski@netfoundry.io>
2020-01-24 17:53:44 -0500

    fixed logs

--- quickstarts/docs/python/nf_network.py
+++ quickstarts/docs/python/nf_network.py
@@ -69,7 +69,8 @@ def create_network(env, name, token):
 def find_network(env, name, token):
     url = 'https://gateway.' + env + '.netfoundry.io/rest/v1/networks'
     networks = nfreq.get_data(url, token)['_embedded']['networks']
-    writelog('\nNetworks found are\n' + networks)
+    writelog('\nNetworks found are\n')
+    writelog(networks)
     for network in networks:
         if network['name'] == name:
             net_url = (network['_links']['self']['href'])
